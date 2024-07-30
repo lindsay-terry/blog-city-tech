a953df277d14e5672e13f8889bb56a536e32a3e8
Lindsay Terry <lindsaytee66@gmail.com>
2024-07-30 16:21:52 -0600

    Updates home routes and modifies path in authorization.js

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,17 +4,37 @@ const auth = require('../utils/authorization');
 
 //route to homepage
 router.get('/', async (req, res) => {
-    res.render('homepage');
-})
+    try { //get all post data to display on page
+        const postData = await Post.findAll({
+            include: [{ model: Comment }, { model: User }],
+        })
+        const posts = postData.map(post => post.get({ plain: true }));
+        
+        res.render('homepage', { posts, logged_in: req.session.logged_in });
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
 
 //route to login page
 router.get('/login', async (req, res) => {
-    res.render('login');
-})
+    try {
+        res.render('login', { logged_in: req.session.logged_in });
+    } catch (error) {
+        res.status(500).json(error);
+    }
+
+});
 
 //route to dashboard
 router.get('/dashboard', auth, async (req, res) => {
-    res.render('dashboard');
-})
+    try {
+    
+        res.render('dashboard', { logged_in: req.session.logged_in });
+    } catch (error) {
+        res.status(500).json(error);
+    }
+    
+});
 
 module.exports = router;
--- utils/authorization.js
+++ utils/authorization.js
@@ -1,7 +1,7 @@
 //custom middleware to check logged in status of user and redirect based on result
 const auth = (req, res, next) => {
     if (!req.session.logged_in) {
-        res.redirect('/api/login');
+        res.redirect('/login');
     } else {
         next();
     }
